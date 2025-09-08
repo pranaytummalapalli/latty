2ef0de717d5ab72ac764353d3fb1eda1b572c28c
pranaytummalapalli <pranayrao12@gmail.com>
2025-09-08 22:22:22 +0530

    working subscriber to read PoseStamped from Manual Control

--- src/latty_chassis/include/latty_chassis/move_latty.hpp
+++ src/latty_chassis/include/latty_chassis/move_latty.hpp
@@ -4,6 +4,8 @@
 #include "gazebo_msgs/srv/set_entity_state.hpp"
 #include "gazebo_msgs/msg/entity_state.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "rclcpp/qos.hpp"
 
 class MoveLatty : public rclcpp::Node
 {
@@ -12,11 +14,20 @@ public:
 
 private:
     void move_latty();
+    void PoseStampedToEntityState(const geometry_msgs::msg::PoseStamped::SharedPtr pose_msg);
 
-    double x_, y_;
+    double x_ = 0.0;
+    double y_ = 0.0;
+    double z_ = 0.0;
+    double q_x_ = 0.0;
+    double q_y_ = 0.0;
+    double q_z_ = 0.0;
+    double q_w_ = 0.0;
 
+    rclcpp::QoS qos_;
     gazebo_msgs::msg::EntityState state_;
     std::shared_ptr<gazebo_msgs::srv::SetEntityState::Request> request_;
     rclcpp::Client<gazebo_msgs::srv::SetEntityState>::SharedPtr client_;
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
 };
--- src/latty_chassis/src/move_latty.cpp
+++ src/latty_chassis/src/move_latty.cpp
@@ -3,8 +3,16 @@
 using namespace std::chrono_literals;
 
 MoveLatty::MoveLatty() 
-    : Node("move_latty"), x_(0.0), y_(0.0)
+    : Node("move_latty"),
+        qos_(rclcpp::QoS(rclcpp::KeepLast(10))
+                .reliability(rclcpp::ReliabilityPolicy::Reliable)
+                .durability(rclcpp::DurabilityPolicy::Volatile))
 {
+    pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>
+                    ("Latty/TargetPose",qos_,
+                    std::bind(&MoveLatty::PoseStampedToEntityState, this, std::placeholders::_1));
+
+
     // Create service client
     client_ = this->create_client<gazebo_msgs::srv::SetEntityState>("/gazebo/set_entity_state");
 
@@ -14,7 +22,25 @@ MoveLatty::MoveLatty()
     }
 
     // Timer to move the robot every 100ms
-    timer_ = this->create_wall_timer(100ms, std::bind(&MoveLatty::move_latty, this));
+    timer_ = this->create_wall_timer(10ms, std::bind(&MoveLatty::move_latty, this));
+}
+
+void MoveLatty::PoseStampedToEntityState(const geometry_msgs::msg::PoseStamped::SharedPtr pose_msg)
+{
+    x_ = pose_msg->pose.position.x;
+    y_ = pose_msg->pose.position.y;
+    z_ = pose_msg->pose.position.z;
+
+    q_x_ = pose_msg->pose.orientation.x;
+    q_y_ = pose_msg->pose.orientation.y;
+    q_z_ = pose_msg->pose.orientation.z;
+    q_w_ = pose_msg->pose.orientation.w;
+
+    RCLCPP_INFO(
+        this->get_logger(),
+        "Received PoseStamped -> (x = %.2f, y = %.2f, z = %.2f, qx = %.2f, qy = %.2f, qz = %.2f, qw = %.2f)",
+        x_, y_, z_, q_x_, q_y_, q_z_, q_w_);
+
 }
 
 void MoveLatty::move_latty()
@@ -25,11 +51,11 @@ void MoveLatty::move_latty()
     state_.name = "warehouse_robot";   // must match the model name in Gazebo
     state_.pose.position.x = x_;
     state_.pose.position.y = y_;
-    state_.pose.position.z = 0.0;
-    state_.pose.orientation.x = 0.0;
-    state_.pose.orientation.y = 0.0;
-    state_.pose.orientation.z = 0.0;
-    state_.pose.orientation.w = 1.0;
+    state_.pose.position.z = z_;
+    state_.pose.orientation.x = q_x_;
+    state_.pose.orientation.y = q_y_;
+    state_.pose.orientation.z = q_z_;
+    state_.pose.orientation.w = q_w_;
 
     request_->state = state_;
 
@@ -47,8 +73,4 @@ void MoveLatty::move_latty()
                 RCLCPP_ERROR(this->get_logger(), "Service call failed: %s", e.what());
             }
         });
-
-    // Increment x and y for demo
-    x_ += 0.0;
-    y_ += 0.05;
 }
